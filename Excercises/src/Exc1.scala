22bdb30a6f4e98efcef9a1890783e3d618adba5d
unknown <Surikat@Surikat-PC.(none)>
2014-08-29 23:51:26 +0300

    initial@

---
+++ Excercises/src/Exc1.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by Surikat on 29.08.14.
+ */
+object Exc1 extends App {
+
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(index: Int): Boolean = {
+      if(index + 1 >= as.length) true
+      else if(gt(as(index), as(index+1))) go(index + 1)
+      else false
+    }
+    go(0)
+  }
+
+  val arr = Array(1,2,3,4,5)
+  println(isSorted(arr, (x: Int, y: Int) => x < y))
+
+}
---
+++ Excercises/src/Exc2.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by Surikat on 29.08.14.
+ */
+object Exc2 {
+
+}
