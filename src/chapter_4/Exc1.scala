d274b8b0e7b23ee1c7cf017692b99a6a28b3c938
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2014-10-23 12:22:02 +0300

    chapter 4 upd

--- src/chapter_4/Exc1.scala
+++ src/chapter_4/Exc1.scala
@@ -1,5 +1,15 @@
 package chapter_4
 
-object Exc1 extends App {
+import chapter_4.auxiliary.{MyStream => ms}
 
+object Exc1 extends App {
+  def pair(i: => Int) = (i, i)
+
+  ms.cons(1, ms(1, ms.empty))
+  pair {println("hello"); 41 + 1}
+
+  val x = ms(1, 3, 4, 6)
+  println(x.toList)
+  println(x.take(2).toList)
+  println(x.takeWhile(_ < 4 ).toList)
 }
--- src/chapter_4/auxiliary/MyStream.scala
+++ src/chapter_4/auxiliary/MyStream.scala
@@ -1,14 +1,85 @@
 package chapter_4.auxiliary
 
 
-sealed trait MyStream[+A] {
+sealed trait MyStream[+A] { self =>
   def uncons: Option[(A, MyStream[A])]
-  def isEmpty: Boolean = mycons.isEmpty
+  def isEmpty: Boolean = uncons.isEmpty
+
+  def toListRec: List[A] = uncons match {
+    case Some((h, t)) => h :: t.toList
+    case None => List()
+  }
+
+  def toList: List[A] = {
+    @annotation.tailrec
+    def aux(init: MyStream[A], rest: List[A]): List[A] = init.uncons match {
+      case Some((h, t)) => aux(t, h :: rest)
+      case _ => rest
+    }
+    aux(self, List()).reverse
+  }
+
+  def take(n: Int): MyStream[A] = {
+
+    def aux(init: MyStream[A], rest: MyStream[A], aggr: Int): MyStream[A] =
+      if (aggr >= n) rest
+      else init.uncons match {
+        case Some((h, t)) => MyStream.cons(h, aux(t, rest, aggr + 1))
+        case _ => rest
+      }
+    aux(self, MyStream(), 0)
+  }
+
+
+  def takeWhile(f: A => Boolean): MyStream[A] = {
+
+    def aux(init: MyStream[A], rest: MyStream[A]): MyStream[A] = init.uncons match {
+      case Some((h, t)) => if(f(h)) MyStream.cons(h, aux(t, rest)) else rest
+      case _ => rest
+    }
+    aux(self, MyStream())
+  }
+
+
+// tail recursive analogues, though working twice as long, due to reversing
+
+//  def take(n: Int): MyStream[A] = {
+//    @annotation.tailrec
+//    def aux(init: MyStream[A], rest: MyStream[A], aggr: Int): MyStream[A] =
+//      if (aggr >= n) rest
+//      else init.uncons match {
+//        case Some((h, t)) => aux(t, MyStream.cons(h, rest), aggr + 1)
+//        case _ => rest
+//      }
+//    aux(self, MyStream(), 0).reverse
+//  }
+//  def takeWhile(f: A => Boolean): MyStream[A] = {
+//    @annotation.tailrec
+//    def aux(init: MyStream[A], rest: MyStream[A]): MyStream[A] = init.uncons match {
+//      case Some((h, t)) => if(f(h)) aux(t, MyStream.cons(h, rest)) else rest
+//      case _ => rest
+//    }
+//    aux(self, MyStream()).reverse
+//  }
+//  private def reverse(): MyStream[A] = {
+//    @annotation.tailrec
+//    def aux(init: MyStream[A], rest: MyStream[A]): MyStream[A] = init.uncons match {
+//      case Some((h, t)) => aux(t, MyStream.cons(h, rest))
+//      case _ => rest
+//    }
+//
+//    aux(self, MyStream())
+//  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = uncons match {
+    case Some((h, t)) => f(h, t.foldRight(z)(f))
+    case None => z
+  }
 }
 
 object MyStream {
 
-  def empty[A]: Stream[A] =
+  def empty[A]: MyStream[A] =
     new MyStream[A] { def uncons = None }
 
   def cons[A](hd: => A, tl: => MyStream[A]): MyStream[A] =
