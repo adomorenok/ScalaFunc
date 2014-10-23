b0e82b2c51a4901b50b3e96e45feb335385ae9a1
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2014-10-23 18:20:59 +0300

    chapter 5 updates

--- src/chapter_4/Exc1.scala
+++ src/chapter_4/Exc1.scala
@@ -9,7 +9,38 @@ object Exc1 extends App {
   pair {println("hello"); 41 + 1}
 
   val x = ms(1, 3, 4, 6)
+  print(" TO LIST ")
   println(x.toList)
+  print(" TAKE ")
   println(x.take(2).toList)
+  print(" TAKE WHILE ")
   println(x.takeWhile(_ < 4 ).toList)
+  print(" EXISTS ")
+  println(x.exists(_ == 3))
+  print(" TAKE WHILE THROUGH FOLD RIGHT ")
+  println(x.takeWhileFR(_ < 4).toList)
+  print(" FILTER ")
+  println(x.filter(_ != 3).toList)
+  print(" APPEND ")
+  println(x.appendL(9).toList)
+
+  val ones = ms.constant(1)
+
+  print(" INFINITE STREAM TAKE WHILE ")
+  println(ones.takeWhile(_ == 1))
+
+  val from3 = ms.fromUnf(3)
+  print(" INFINITE STREAM FROM 3 TAKE 5 ")
+  println(from3.take(5).toList)
+
+  val fbn = ms.fibs
+
+  print(" INFINITE STREAM FIBONACCI TAKE 8 ")
+  println(fbn.take(8).toList)
+
+  val cstUnf = ms.constantUnf(3)
+  print(" INFINITE STREAM CONSTANT 3 TAKE 8 ")
+  println(cstUnf.take(8).toList)
+
+
 }
--- src/chapter_4/auxiliary/MyStream.scala
+++ src/chapter_4/auxiliary/MyStream.scala
@@ -75,6 +75,35 @@ sealed trait MyStream[+A] { self =>
     case Some((h, t)) => f(h, t.foldRight(z)(f))
     case None => z
   }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  def takeWhileFR(f: A => Boolean): MyStream[A] =
+    foldRight(MyStream.empty[A])((h, t) =>
+      if(f(h)) MyStream.cons(h, t)
+      else MyStream.empty)
+
+  def map[B](f: A => B): MyStream[B] =
+    foldRight(MyStream.empty[B])((h, t) => MyStream.cons(f(h), t))
+
+  def filter(f: A => Boolean): MyStream[A] =
+    foldRight(MyStream.empty[A]) ((h, t) => if(f(h)) MyStream.cons(h, t) else t)
+
+  def append[B>:A](s: => MyStream[B]): MyStream[B] =
+    foldRight(s)((h, t) => MyStream.cons(h, t))
+
+  def appendL[B>:A](s: => B): MyStream[B] =
+    foldRight(MyStream.cons(s, MyStream.empty))((h, t) => MyStream.cons(h, t))
+
+  def flatMap[B](f: A => MyStream[B]): MyStream[B] =
+    foldRight(MyStream.empty[B])((h, t) => f(h) append t)
+
+//  def mapUnf
+
 }
 
 object MyStream {
@@ -90,4 +119,35 @@ object MyStream {
   def apply[A](as: A*): MyStream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+
+  def constant[A](a: A): MyStream[A] = cons(a, constant(a))
+
+  def from(n: Int): MyStream[Int] = cons(n, from(n+1))
+
+  def fibs: MyStream[Int] = {
+    def aux(prv: Int, nxt: Int): MyStream[Int] = cons(prv, aux(nxt, prv + nxt))
+    aux(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): MyStream[A] =
+    f(z) match {
+      case Some((h, t)) => cons(h, unfold(t)(f))
+      case None => empty
+    }
+
+  def fibsUnf: MyStream[Int] =
+    unfold((0, 1)) {
+      case (f0,f1) => Some(
+        (f0, (f1, f0 + f1))
+      )
+    }
+
+  def fromUnf(n: Int): MyStream[Int] =
+    unfold(n)((x) => Some((x, x + 1)))
+
+  def constantUnf(n: Int): MyStream[Int] =
+    unfold(n)((x) => Some((x, x)))
+
+  def onesUnf: MyStream[Int] = unfold(1)(x => Some((1, 1)))
+
 }
